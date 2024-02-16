7ccc5f2915f58c4a50267c558c3555f5937bbdc4
GustavoF3rnandes <gustavof3rnandes@outlook.com>
2024-02-16 17:03:25 -0300

    update app

--- api/app.py
+++ api/app.py
@@ -41,7 +41,7 @@ def send():
         msg = Message(
             subject = f'{formContato.nome} te enviou uma mensagem no portfólio',
             sender = app.config.get("MAIL_USERNAME"),
-            recipients= ['docencia.thiago@gmail.com', app.config.get("MAIL_USERNAME")],
+            recipients= ['gustavof3rnandes@outlook.com', app.config.get("MAIL_USERNAME")],
             body = f'''
             
             {formContato.nome} com o e-mail {formContato.email}, te enviou a seguinte mensagem:
@@ -55,4 +55,4 @@ def send():
     return redirect('/')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
